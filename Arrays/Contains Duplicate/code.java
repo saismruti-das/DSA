9682ab2a32d9560a631270d57569e0e58a021669
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-07-20 17:31:59 +0530

    Create code.java

---
+++ Arrays/Contains Duplicate/code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map=new HashSet<>();
+        for (int num: nums){
+            if (map.contains(num)){
+                return true;
+            }
+            else{
+                map.add(num);
+            }
+        }
+        return false;
+    }
+}
