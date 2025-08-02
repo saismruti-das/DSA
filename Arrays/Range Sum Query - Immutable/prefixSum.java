d2767f167812d09a13b4e4f8a732a8d9aead208f
Saismruti Das <104313895+saismruti-das@users.noreply.github.com>
2025-08-02 13:15:16 +0530

    Create prefixSum.java

---
+++ Arrays/Range Sum Query - Immutable/prefixSum.java
@@ -0,0 +1,26 @@
+class NumArray {
+    int[] nums;
+
+    public NumArray(int[] nums) {
+        //modify array to prefix-sum array
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return nums[right];
+        } else {
+            return nums[right] - nums[--left];
+        }
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
