6ed123037a96fe0ca2705a97a9a76e13447605ad
saismruti-das <das.sai1312@gmail.com>
2025-08-26 04:04:58 +0530

    Binary Search

---
+++ PracticeCodes/Binary Search/BinarySearch.java
@@ -0,0 +1,45 @@
+//given an array of elements and a target element, find the index of the target element in the array
+
+import java.util.*;
+class BinarySearch{
+
+    static int binarySearch(int[] arr, int x){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid =(int) (start+end)/2;
+
+            if(arr[mid]==x)
+            return mid;
+
+            else if(x > arr[mid])     //x is present in right side
+                start=mid+1;
+
+            else
+            end=mid-1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        //size of array
+        System.out.println("Enter size of array");
+        int n=sc.nextInt();
+
+        //input array
+        int[] arr=new int[n];
+        System.out.println("Enter elements of array");
+        for (int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+        //target element
+        System.out.println("Enter target elemet");
+        int target=sc.nextInt();
+
+        System.out.print("Index of target: "+binarySearch(arr, target));
+    }
+}
