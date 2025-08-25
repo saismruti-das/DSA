08711df88f358f88ff1727ebb34af08688b0eb24
saismruti-das <das.sai1312@gmail.com>
2025-08-25 22:02:41 +0530

    Knapsack memoization

--- PracticeCodes/DP/knapsackMemoization.java
+++ PracticeCodes/DP/knapsackMemoization.java
@@ -25,41 +25,41 @@ public class knapsackMemoization {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         // Input: number of items
-        System.out.print("Enter number of items: ");
-        int n = sc.nextInt();
-
-        int[] values = new int[n];
-        int[] weights = new int[n];
-
-        // Input: values
-        System.out.println("Enter values of items:");
-        for (int i = 0; i < n; i++) {
-            values[i] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            // Input: number of items
+            System.out.print("Enter number of items: ");
+            int n = sc.nextInt();
+            
+            int[] values = new int[n];
+            int[] weights = new int[n];
+            
+            // Input: values
+            System.out.println("Enter values of items:");
+            for (int i = 0; i < n; i++) {
+                values[i] = sc.nextInt();
+            }
+            
+            // Input: weights
+            System.out.println("Enter weights of items:");
+            for (int i = 0; i < n; i++) {
+                weights[i] = sc.nextInt();
+            }
+            
+            // Input: capacity of knapsack
+            System.out.print("Enter knapsack capacity: ");
+            int W = sc.nextInt();
+            
+            //initialize table
+            table = new int[n+1][W+1];
+            for (int i=0; i<n+1; i++){
+                Arrays.fill(table[i],-1);
+            }
+            
+            // Call knapsack function
+            int result = knapsack(weights, values, W, n);
+            
+            System.out.println("Maximum value = " + result);
         }
-
-        // Input: weights
-        System.out.println("Enter weights of items:");
-        for (int i = 0; i < n; i++) {
-            weights[i] = sc.nextInt();
-        }
-
-        // Input: capacity of knapsack
-        System.out.print("Enter knapsack capacity: ");
-        int W = sc.nextInt();
-
-        //initialize table
-        table = new int[n+1][W+1];
-        for (int i=0; i<n+1; i++){
-            Arrays.fill(table[i],-1);
-        }
-
-        // Call knapsack function
-        int result = knapsack(weights, values, W, n);
-
-        System.out.println("Maximum value = " + result);
-        sc.close();
     }
 }
