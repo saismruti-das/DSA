0c30d7849a1f67a227746c7c92764b6af340a457
saismruti-das <das.sai1312@gmail.com>
2025-08-26 04:24:30 +0530

    Order Agnostic BSearch

---
+++ PracticeCodes/Binary Search/OrderAgnosticBSearch.java
@@ -0,0 +1,69 @@
+//given an sorted array (ascending or decending) of elements and a target element, find the index of the target element in the array
+
+import java.util.*;
+class OrderAgnosticBSearch{
+
+    static int binarySearchReverseSorted(int[] arr, int x){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid =(int) (start+end)/2;
+
+            if(arr[mid]==x)
+            return mid;
+
+            else if(x < arr[mid])     //x is present in right side
+                start=mid+1;
+
+            else
+            end=mid-1;
+        }
+        return -1;
+    }
+
+    static int binarySearchSorted(int[] arr, int x){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid =(int) (start+end)/2;
+
+            if(arr[mid]==x)
+            return mid;
+
+            else if(x > arr[mid])     //x is present in right side
+                start=mid+1;
+
+            else
+            end=mid-1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        //size of array
+        try (Scanner sc = new Scanner (System.in)) {
+            //size of array
+            System.out.println("Enter size of array");
+            int n=sc.nextInt();
+            
+            //input array
+            int[] arr=new int[n];
+            System.out.println("Enter elements of array");
+            for (int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            
+            //target element
+            System.out.println("Enter target elemet");
+            int target=sc.nextInt();
+            
+            if(arr[0]<arr[n-1])
+            System.out.print("Index of target: "+binarySearchSorted(arr, target));
+            
+            else
+            System.out.print("Index of target: "+binarySearchReverseSorted(arr, target));
+        }
+    }
+}
